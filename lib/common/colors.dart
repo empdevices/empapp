26b8f54090e5894c982a96655a934b8af6aea402
Prince Tetteh <princetetteh963@gmail.com>
2024-08-01 23:47:46 +0000

    color scheme changed

--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-Color mainCol =  const Color(0xFF0F0159);
+Color mainCol = const Color(0xFF0F0159);
 
-Color textGrey = Colors.black45;
+Color textGrey = Color.fromARGB(115, 0, 0, 0);
 Color appGrey = const Color(0xFFF1F1F1);
 Color appGreen = const Color(0xFF70E000);
